2fca7419dec7902492a33a378a5d26466611e976
Lucas <lfuser@gmail.com>
2016-09-10 11:46:57 -0400

    Adicionando arquivos iniciais.

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.sum = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.a6 = new System.Windows.Forms.Button();
+            this.a9 = new System.Windows.Forms.Button();
+            this.a5 = new System.Windows.Forms.Button();
+            this.a8 = new System.Windows.Forms.Button();
+            this.a4 = new System.Windows.Forms.Button();
+            this.a7 = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.White;
+            this.a1.Location = new System.Drawing.Point(84, 635);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(42, 31);
+            this.a1.TabIndex = 1;
+            this.a1.Text = "1";
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.a1_Click);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.Transparent;
+            this.a2.Location = new System.Drawing.Point(146, 635);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(42, 31);
+            this.a2.TabIndex = 3;
+            this.a2.Text = "2";
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.a2_Click);
+            // 
+            // sum
+            // 
+            this.sum.BackColor = System.Drawing.Color.Transparent;
+            this.sum.Location = new System.Drawing.Point(266, 635);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(44, 31);
+            this.sum.TabIndex = 4;
+            this.sum.Text = "sum";
+            this.sum.UseVisualStyleBackColor = false;
+            this.sum.Click += new System.EventHandler(this.sum_Click);
+            // 
+            // a3
+            // 
+            this.a3.Location = new System.Drawing.Point(206, 635);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(46, 31);
+            this.a3.TabIndex = 5;
+            this.a3.Text = "3";
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.a3_Click);
+            // 
+            // a6
+            // 
+            this.a6.Location = new System.Drawing.Point(206, 580);
+            this.a6.Name = "a6";
+            this.a6.Size = new System.Drawing.Size(46, 31);
+            this.a6.TabIndex = 6;
+            this.a6.Text = "6";
+            this.a6.UseVisualStyleBackColor = true;
+            this.a6.Click += new System.EventHandler(this.a6_Click);
+            // 
+            // a9
+            // 
+            this.a9.Location = new System.Drawing.Point(206, 528);
+            this.a9.Name = "a9";
+            this.a9.Size = new System.Drawing.Size(46, 29);
+            this.a9.TabIndex = 7;
+            this.a9.Text = "9";
+            this.a9.UseVisualStyleBackColor = true;
+            this.a9.Click += new System.EventHandler(this.a9_Click);
+            // 
+            // a5
+            // 
+            this.a5.Location = new System.Drawing.Point(146, 580);
+            this.a5.Name = "a5";
+            this.a5.Size = new System.Drawing.Size(42, 31);
+            this.a5.TabIndex = 8;
+            this.a5.Text = "5";
+            this.a5.UseVisualStyleBackColor = true;
+            this.a5.Click += new System.EventHandler(this.a5_Click);
+            // 
+            // a8
+            // 
+            this.a8.Location = new System.Drawing.Point(146, 527);
+            this.a8.Name = "a8";
+            this.a8.Size = new System.Drawing.Size(42, 30);
+            this.a8.TabIndex = 9;
+            this.a8.Text = "8";
+            this.a8.UseVisualStyleBackColor = true;
+            this.a8.Click += new System.EventHandler(this.a8_Click);
+            // 
+            // a4
+            // 
+            this.a4.Location = new System.Drawing.Point(84, 580);
+            this.a4.Name = "a4";
+            this.a4.Size = new System.Drawing.Size(42, 31);
+            this.a4.TabIndex = 10;
+            this.a4.Text = "4";
+            this.a4.UseVisualStyleBackColor = true;
+            this.a4.Click += new System.EventHandler(this.a4_Click);
+            // 
+            // a7
+            // 
+            this.a7.Location = new System.Drawing.Point(84, 527);
+            this.a7.Name = "a7";
+            this.a7.Size = new System.Drawing.Size(42, 31);
+            this.a7.TabIndex = 11;
+            this.a7.Text = "7";
+            this.a7.UseVisualStyleBackColor = true;
+            this.a7.Click += new System.EventHandler(this.a7_Click);
+            // 
+            // equals
+            // 
+            this.equals.Location = new System.Drawing.Point(326, 691);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(45, 31);
+            this.equals.TabIndex = 12;
+            this.equals.Text = "equal";
+            this.equals.UseVisualStyleBackColor = true;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(102, 170);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(220, 44);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.calc2;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(468, 784);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 778);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.a7);
+            this.Controls.Add(this.a4);
+            this.Controls.Add(this.a8);
+            this.Controls.Add(this.a5);
+            this.Controls.Add(this.a9);
+            this.Controls.Add(this.a6);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button a6;
+        private System.Windows.Forms.Button a9;
+        private System.Windows.Forms.Button a5;
+        private System.Windows.Forms.Button a8;
+        private System.Windows.Forms.Button a4;
+        private System.Windows.Forms.Button a7;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Drawing.Text;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            //InitCustomLabelFont();
+
+            Controls.Add(a1);
+            Controls.Add(a2);
+            Controls.Add(a3);
+            Controls.Add(a4);
+            Controls.Add(a5);
+            Controls.Add(a6);
+            Controls.Add(a7);
+            Controls.Add(a8);
+            Controls.Add(a9);
+            Controls.Add(sum);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "1";
+        }
+
+        private void a2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "2";
+        }
+
+        private void a3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "3";
+        }
+
+        private void a4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "4";
+        }
+
+        private void a5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "5";
+        }
+
+        private void a6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "6";
+        }
+
+        private void a7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "7";
+        }
+
+        private void a8_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "8";
+        }
+
+        private void a9_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "9";
+        }
+
+        private void sum_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "+";
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //public void InitCustomLabelFont()
+        //{
+        //    PointF pointF = new PointF(10, 0);
+        //    SolidBrush solidBrush = new SolidBrush(Color.Black);
+
+        //    int count = 0;
+        //    string familyName = "";
+        //    string familyNameAndStyle;
+        //    FontFamily[] fontFamilies;
+        //    PrivateFontCollection privateFontCollection = new PrivateFontCollection();
+
+        //    privateFontCollection.AddFontFile("c:\users\jho\documents\visual studio 2013\Projects\WindowsFormsApplication1\WindowsFormsApplication1\Resources\The Display St.ttf");
+
+        //    richTextBox1.Font = new Font(privateFontCollection.Families[0], richTextBox1.Font.Size);
+        //}
+    }
+}
